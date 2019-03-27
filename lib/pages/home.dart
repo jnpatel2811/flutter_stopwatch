03d51e196f41ca428d892dd3593151650e74ada3
Jay Patel <jnpatel2811@gmail.com>
2019-03-27 18:30:45 +0530

    some refactoring

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,25 +9,40 @@ class HomeWidget extends StatefulWidget {
 
 class _HomeWidgetState extends State<HomeWidget>
     with SingleTickerProviderStateMixin {
-  String _stopwatchText = "00:00:00";
-  String _buttonText = _textSTART;
-
-  final _stopWatch = new Stopwatch();
-  final _timeout = const Duration(milliseconds: 5);
-
   static final _textSTOP = "Stop";
   static final _textSTART = "Start";
+
   static final _textReset = "Reset";
+  String _stopwatchText = "00:00:00";
+
+  String _buttonText = _textSTART;
+  final _stopWatch = new Stopwatch();
+  final _timeout = const Duration(milliseconds: 5);
 
   AnimationController _animationController;
   Animation _colorAnimation;
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stopwatch"),
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _animationController.dispose();
+  }
 
   @override
   void initState() {
     super.initState();
     _animationController =
-        AnimationController(vsync: this, duration: Duration(seconds: 1));
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
     _colorAnimation =
         ColorTween(begin: Color(0xff3CA55C), end: Color(0xffB5AC49))
             .animate(_animationController);
@@ -41,30 +56,9 @@ class _HomeWidgetState extends State<HomeWidget>
     });
   }
 
-  @override
-  void dispose() {
-    super.dispose();
-    _animationController.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Stopwatch"),
-      ),
-      body: _buildBody(),
-    );
-  }
-
   Widget _buildBody() {
     return Container(
       color: _colorAnimation.value,
-      /*decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Color(0xff3CA55C), Color(0xffB5AC49)],
-            begin: Alignment.bottomCenter,
-          )),*/
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
